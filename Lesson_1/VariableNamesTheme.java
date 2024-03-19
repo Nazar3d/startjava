99a16db946cbf797d86c277fa9432baacb79f7a7
Nazar <104254764+Nazar3d@users.noreply.github.com>
2024-03-19 21:04:11 +0300

    Add files via upload

---
+++ Lesson_1/VariableNamesTheme.java
@@ -0,0 +1,113 @@
+public class VariableNamesTheme {
+    public static void main(String[] args) {
+
+        //Разные переменные
+        int digit = 9;
+        int sumOfNumbers = 18;
+        int productOfDigits = 81;
+        int maximumNumber = 999;
+        int numberOfTens = 2;
+        int dogWeight = 24;
+        int initialNumber = 1;
+        int depositPercentage = 19;
+        char symbol = 0026;
+        int errorCode = 404;
+        String messageType = "Printed messages";
+        int numberOfZeros = 1;
+        int uniqueNumber = 4554;
+        int randomNumber = 759;
+        String mathematicalExpression = "999 + 111 = 1110";
+        int selectionOfSomething = 10;
+        int numberOfPoints = 5121;
+        int maximumLineLengthMenu = 900000;
+        int item = 120;
+        int costOfCoffeeToTakeAway = 100;
+        String startDate = "22/06/1941";
+        int endOfTheRange = 20;
+        String fullNameOfTheEmployeeMonth = "Sergey Dmitrievich Goremykin";
+        int eBookTitle = 9;
+        int size = 19;
+        int capacity = 200;
+        int counter = 1231;
+        String filePath = "C:\\BeamNG.drive\\content\\vehicles\\chrysler_300c.zip";
+        int numberOfNumbersInEachLine = 80;
+
+        //boolean-переменные
+        boolean hundredsAreEqual = true;
+        boolean theComputerIsTurned = true;
+        boolean onThereAreEqualNumbers = false;
+        boolean created = true;
+        boolean empty = false;
+        boolean active = false;
+        boolean news = true;
+        boolean emailValid = true;
+        boolean thereAreUniqueStrings = false;
+
+        // Аббревиатуры
+        String oldUuid = "универсально уникальный идентификатор";
+        String manufacturerOfRam = "hyperX";
+        String hardDiskCapacity = "4тб";
+        String http = "протокол передачи гипертекста";
+        String abbreviatedUrl = "единый локатор ресурсов";
+        String newClientId = "Идентификатор клиента — это уникальный код, который присваивается каждому пользователю в системе";
+        String encodingAscfi = "американский стандартный код для обмена информацией";
+
+        System.out.println("1. Разные переменные");
+        System.out.println();
+        System.out.println("цифра = " + digit);
+        System.out.println("сумма чисел = " + sumOfNumbers);
+        System.out.println("произведение цифр = " + productOfDigits);
+        System.out.println("максимальное число = " + maximumNumber);
+        System.out.println("количество десятков = " + numberOfTens);
+        System.out.println("вес собаки = " + dogWeight);
+        System.out.println("исходное число = " + initialNumber);
+        System.out.println("процент по вкладу = " + depositPercentage + "%");
+        System.out.println("символ = " + symbol);
+        System.out.println("код ошибки = " + errorCode);
+        System.out.println("тип сообщения = " + messageType);
+        System.out.println("число нулей = " + numberOfZeros);
+        System.out.println("уникальное число = " + uniqueNumber);
+        System.out.println("случайное число = " + randomNumber);
+        System.out.println("математическое выражение = " + mathematicalExpression);
+        System.out.println("выбор = " + selectionOfSomething);
+        System.out.println("количество очков = " + numberOfPoints);
+        System.out.println("максимальная длина строки = " + maximumLineLengthMenu);
+        System.out.println("пункт меню = " + item);
+        System.out.println("стоимость кофе на вынос = " + costOfCoffeeToTakeAway);
+        System.out.println("дата начала = " + startDate);
+        System.out.println("окончание диапазона = " + endOfTheRange);
+        System.out.println("полное имя работника месяца = " + fullNameOfTheEmployeeMonth);
+        System.out.println("заголовок электронной книги = " + eBookTitle);
+        System.out.println("размер = " + size);
+        System.out.println("вместимость = " + capacity + "л");
+        System.out.println("счетчик = " + counter);
+        System.out.println("путь до файла = " + filePath);
+        System.out.println("количество чисел в каждой строке = " +numberOfNumbersInEachLine);
+        System.out.println();
+        System.out.println();
+
+        System.out.println("2. Boolean-переменные");
+        System.out.println();
+        System.out.println("сотни равны = " + hundredsAreEqual);
+        System.out.println("компьютер включен = " + theComputerIsTurned);
+        System.out.println("есть равные цифры = " + onThereAreEqualNumbers);
+        System.out.println("создано = " + created);
+        System.out.println("пусто = " + empty);
+        System.out.println("активный = " + active);
+        System.out.println("новый = " + news);
+        System.out.println("электронная почта действительная = " + emailValid);
+        System.out.println("имеются уникальные строки = " + thereAreUniqueStrings);
+        System.out.println();
+        System.out.println();
+
+        System.out.println("3. Аббревиатуры");
+        System.out.println();
+        System.out.println("старый universally unique identifier = " + oldUuid);
+        System.out.println("производитель оперативной памяти = " + manufacturerOfRam);
+        System.out.println("емкость жесткого диска = " + hardDiskCapacity);
+        System.out.println("протокол передачи гипертекста = " + http);
+        System.out.println("сокращенный uniform resource locator = " + abbreviatedUrl);
+        System.out.println("новый идентификатор клиента = " + newClientId);
+        System.out.println("кодировка american standard code for information interchange = " + encodingAscfi);
+    }
+}
